58d41562e3ef506285031bd3e9021236241d0235
Finn Gebicke <fige.gebicke@gmail.com>
2025-07-06 02:01:05 +0200

    Working navigationBar added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 import BottomNavigation from "./components/BottomNavigation";
 function App() {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="h-screen flex">
       <BottomNavigation />
     </div>
   );
